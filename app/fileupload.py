a617c3a29c8fe43a43d0d5c1a8e7443f07a6a193
lunarss <woshiyinhao@gmail.com>
2020-10-18 20:56:32 -0400

    Some updates

--- app/fileupload.py
+++ app/fileupload.py
@@ -16,29 +16,23 @@ def upload_form():
 @webapp.route('/test/FileUpload',methods=['POST'])
 #Upload a new file and store in the systems temp directory
 def file_upload():
-    userid = request.form.get("userID")
-    passowrd = request.form.get("password")
-
     # check if the post request has the file part
     if 'uploadedfile' not in request.files:
         return "Missing uploaded file"
-
+    
     new_file = request.files['uploadedfile']
 
     # if user does not select file, browser also
     # submit a empty part without filename
     if new_file.filename == '':
         return 'Missing file name'
-
+    
     # A1 codes starts
     # save upload image
     
     
-    
-    
-    
-    
-    
+
+
     
     
     
@@ -47,20 +41,20 @@ def file_upload():
     path = '/home/ubuntu/Desktop/A1/app/'
     # TODO add userID before timestamp
     uploadImgName = datetime.now().strftime("%m%d%Y%H%M%S") + new_file.filename
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
     
     
     
     new_file.save(os.path.join(path,'Upload_Image/', uploadImgName))
+    # check image size
+    if os.stat(path + 'Upload_Image/' + uploadImgName).st_size > (64 * 1024 * 1024):
+        os.system('rm ' + path + 'Upload_Image/' + uploadImgName)
+        return 'File too large'
+        
     # change cwd for linux command execution
     os.chdir(path + 'FaceMaskDetection/')
     # run image test
@@ -97,33 +91,11 @@ def file_upload():
         image_category = 4
         image_class_str = 'some faces wearing masks'
     # delete temporary files
-    os.system('sudo rm -rf tmp/img_info.txt')
-    os.system('mv tmp/test_' + uploadImgName + ' ../Output_Image/test_' + uploadImgName)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # fetch test image path (TODO, error on showing images on the browser)
-    testImgPath = path + 'Output_Image/test_' + uploadImgName
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return render_template('/fileupload/view.html', img = testImgPath, category = image_category, description = image_class_str)
+    os.system('rm tmp/img_info.txt')
+    os.system('mv tmp/test_' + uploadImgName + ' ../static/test_' + uploadImgName)
+    # fetch test image name
+    testImgName = 'test_' + uploadImgName
+    return render_template('/fileupload/view.html', img = testImgName, category = image_category, description = image_class_str)
     # A1 codes ends
 
 
